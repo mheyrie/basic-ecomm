c766def84955eb9be6c6b4632331005a33045186
Mheyrie <omota4fidelity@gmail.com>
2024-02-21 00:18:10 +0100

    exercise

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ const twoSubstractCart = document.querySelector('.substract-by-two').addEventLis
 const threeSubstractCart = document.querySelector('.substract-by-three').addEventListener('click', substractThreeFromCart);
 const resetCartt = document.querySelector('.reset-cart');
 
-
 // resetCartt.addEventListener('click', ResetCart)
 let cartQuantity = 0;
-
 
 function cardQuantity(){ 
     console.log(`Cart Quantity: ${cartQuantity}`)
@@ -26,7 +24,6 @@ function updateCartQuantity(value){
         cartQuantity += value
         console.log(`Cart Quantity: ${cartQuantity}`)
     }
-    
 }
 
 function addToCart(){
@@ -68,4 +65,32 @@ function resetCart(){
 }
 
 
+// Exercise 8
+const product = {
+    name: "baskekball",
+    price: 2095
+}
 
+const product1 = {
+    name: "book",
+    price: 2000
+}
+
+
+product.price += 500 
+product['delivery-time'] = "3 days"
+product.available = true
+// console.log(product)
+
+function compareProduct(product1, product2){
+    return product1.price<product2.price ? product1.name : product2.name
+}
+
+// console.log(compareProduct(product, product1))
+
+function isSameProduct(product1, product2){
+    return (product1.name === product2.name && product1.price === product2.price) ? true : false
+}
+console.log(isSameProduct(product,product1))
+console.log("Good Morning".toLowerCase())
+console.log("Good Morning".repeat(4))
