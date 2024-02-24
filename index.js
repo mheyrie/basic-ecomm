cfe5a3f8aae81becdd27233bad2d2da1a2e8cc3f
Mheyrie <omota4fidelity@gmail.com>
2024-02-25 00:46:41 +0100

    displays on page

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const cart = document.querySelector('.show-cart').addEventListener('click', cardQuantity);
+const cart = document.querySelector('.show-cart')
 const addCart = document.querySelector('.add-to-cart').addEventListener('click', addToCart);
 const twoAddCart = document.querySelector('.increase-by-two').addEventListener('click', addTwoToCart);
 const threeAddCart = document.querySelector('.increase-by-three').addEventListener('click', addThreeToCart);
@@ -7,23 +7,34 @@ const fiveAddCart = document.querySelector('.increase-by-five').addEventListener
 const twoSubstractCart = document.querySelector('.substract-by-two').addEventListener('click', substractTwoFromCart);
 const threeSubstractCart = document.querySelector('.substract-by-three').addEventListener('click', substractThreeFromCart);
 const resetCartt = document.querySelector('.reset-cart');
+const display = document.querySelector(".show-text")
+
 
 // resetCartt.addEventListener('click', ResetCart)
 let cartQuantity = 0;
 
+//displays cart quantity immediately page loads
+cardQuantity()
+
 function cardQuantity(){ 
-    console.log(`Cart Quantity: ${cartQuantity}`)
-};
+        display.innerHTML = `Cart Quantity: ${cartQuantity}`
+    };
 
 function updateCartQuantity(value){
     if(cartQuantity + value > 10){
         alert("This cart is full, it can only contain 10 items")
+        return;
     }else if(cartQuantity + value < 0){
         alert("Not enough items in the cart")
+        return;
+
     }else{
         cartQuantity += value
-        console.log(`Cart Quantity: ${cartQuantity}`)
+        
+        cardQuantity()
     }
+
+    
 }
 
 function addToCart(){
@@ -59,12 +70,13 @@ function substractThreeFromCart(){
 }
 
 function resetCart(){
-    console.log("Cart was reset")
     cartQuantity = 0
-    console.log(`Cart Quantity: ${cartQuantity}`)
+    console.log("Cart was reset")
+    
+    cardQuantity()
 }
 
-
+/*
 // Exercise 8
 const product = {
     name: "baskekball",
@@ -93,4 +105,4 @@ function isSameProduct(product1, product2){
 }
 console.log(isSameProduct(product,product1))
 console.log("Good Morning".toLowerCase())
-console.log("Good Morning".repeat(4))
+// console.log("Good Morning".repeat(4))*/
